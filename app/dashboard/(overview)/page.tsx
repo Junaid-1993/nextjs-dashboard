170a8a030545adc0f76f568f5418b332c1f25def
junaid-1993 <junaidali9001@gmail.com>
2024-12-08 12:44:34 +0500

    Add Metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,6 +6,12 @@ import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from "@/a
 
 import CardWrapper from "@/app/ui/dashboard/cards";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+
 export default async function Page() {
   return (
     <main>
